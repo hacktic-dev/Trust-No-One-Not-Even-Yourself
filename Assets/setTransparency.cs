eb925c4c303f81498a54b8e0a68baf64fcdedb53
matthew-bvrn <foorganders@outlook.com>
2020-05-30 15:18:54 +0100

    Adjust barrier transparency

--- Assets/setTransparency.cs
+++ Assets/setTransparency.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class setTransparency : MonoBehaviour
 {
     public GameObject currentGameObject;
-    public float alpha = 0.5f;
+    float alpha = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
         currentGameObject = gameObject;
+        Color oldColor = currentGameObject.GetComponent<Renderer>().material.color;
+        currentGameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(oldColor.r, oldColor.g, oldColor.b, alpha));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Color oldColor = currentGameObject.GetComponent<Renderer>().material.color;
-        currentGameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(oldColor.r, oldColor.g, oldColor.b, alpha));
+       
     }
 }
